e8f7abcd8cd5434791761628f03af2abf92ed5a7
GLaDOSaperture <LifeformandDiskOperatingSystem@gmail.com>
2026-08-18 16:47:29 -0500

    updated the project to 2026

---
+++ src/main/java/frc/robot/BuildConstants.java
@@ -0,0 +1,17 @@
+package frc.robot;
+
+/** Automatically generated file containing build version information. */
+public final class BuildConstants {
+    public static final String MAVEN_GROUP = "";
+    public static final String MAVEN_NAME = "ProjectRoar";
+    public static final String VERSION = "unspecified";
+    public static final int GIT_REVISION = 6;
+    public static final String GIT_SHA = "41cfc54350a2ca0b5bc1e3350f450c3d66fb74a2";
+    public static final String GIT_DATE = "2026-08-15 13:33:39 EDT";
+    public static final String GIT_BRANCH = "main";
+    public static final String BUILD_DATE = "2026-08-18 17:41:07 EDT";
+    public static final long BUILD_UNIX_TIME = 1787089267425L;
+    public static final int DIRTY = 1;
+
+    private BuildConstants() {}
+}
